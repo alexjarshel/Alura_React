d61d7e240d607ba60f23ee16b4e299c0023c33e3
unknown <alexbaukartjarschel@gmail.com>
2023-05-12 17:33:58 -0300

    .

---
+++ src/components/Banner/index.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+const Banner = () => {
+    //JSX
+    return (
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="Banner principal"></img>
+        </header>
+    )
+
+}
+
+export default Banner;
---
+++ src/components/CampoTexto/index.js
@@ -0,0 +1,10 @@
+const CampoTexto = () =>{
+    return(
+        <div>
+            <label>nome</label>
+            <input />
+        </div>
+    )
+}
+
+export default CampoTexto;
